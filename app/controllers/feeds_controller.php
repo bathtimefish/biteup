45a16e53f2714f219eaeb37bedd3af8d620a8028
BathTimeFish <bathtimefish@gmail.com>
2012-01-04 10:25:04 +0900

    201201041025

--- app/controllers/feeds_controller.php
+++ app/controllers/feeds_controller.php
@@ -1,160 +1,160 @@
 <?php
 class FeedsController extends AppController {
 
-	var $name = 'Feeds';
-	var $helpers = array('Javascript');
-	var $components = array('Auth', 'WebApi');
-	var $uses = array('Feed', 'Friend', 'Like');
+ var $name = 'Feeds';
+ var $helpers = array('Javascript');
+ var $components = array('Auth', 'WebApi');
+ var $uses = array('Feed', 'Friend', 'Like');
 
-	function index() {
-		$this->Feed->recursive = 0;
-		$this->set('feeds', $this->paginate());
-	}
+ function index() {
+  $this->Feed->recursive = 0;
+  $this->set('feeds', $this->paginate());
+ }
 
-	function view($id = null) {
-		if (!$id) {
-			$this->Session->setFlash(__('Invalid feed', true));
-			$this->redirect(array('action' => 'index'));
-		}
-		$this->set('feed', $this->Feed->read(null, $id));
-	}
+ function view($id = null) {
+  if (!$id) {
+   $this->Session->setFlash(__('Invalid feed', true));
+   $this->redirect(array('action' => 'index'));
+  }
+  $this->set('feed', $this->Feed->read(null, $id));
+ }
 
-	function add() {
-		if (!empty($this->data)) {
-			$this->Feed->create();
-			if ($this->Feed->save($this->data)) {
-				$this->Session->setFlash(__('The feed has been saved', true));
-				$this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(__('The feed could not be saved. Please, try again.', true));
-			}
-		}
-		$users = $this->Feed->User->find('list');
-		$jobs = $this->Feed->Job->find('list');
-		$this->set(compact('users', 'jobs'));
-	}
+ function add() {
+  if (!empty($this->data)) {
+   $this->Feed->create();
+   if ($this->Feed->save($this->data)) {
+    $this->Session->setFlash(__('The feed has been saved', true));
+    $this->redirect(array('action' => 'index'));
+   } else {
+    $this->Session->setFlash(__('The feed could not be saved. Please, try again.', true));
+   }
+  }
+  $users = $this->Feed->User->find('list');
+  $jobs = $this->Feed->Job->find('list');
+  $this->set(compact('users', 'jobs'));
+ }
 
-	function edit($id = null) {
-		if (!$id && empty($this->data)) {
-			$this->Session->setFlash(__('Invalid feed', true));
-			$this->redirect(array('action' => 'index'));
-		}
-		if (!empty($this->data)) {
-			if ($this->Feed->save($this->data)) {
-				$this->Session->setFlash(__('The feed has been saved', true));
-				$this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(__('The feed could not be saved. Please, try again.', true));
-			}
-		}
-		if (empty($this->data)) {
-			$this->data = $this->Feed->read(null, $id);
-		}
-		$users = $this->Feed->User->find('list');
-		$jobs = $this->Feed->Job->find('list');
-		$this->set(compact('users', 'jobs'));
-	}
+ function edit($id = null) {
+  if (!$id && empty($this->data)) {
+   $this->Session->setFlash(__('Invalid feed', true));
+   $this->redirect(array('action' => 'index'));
+  }
+  if (!empty($this->data)) {
+   if ($this->Feed->save($this->data)) {
+    $this->Session->setFlash(__('The feed has been saved', true));
+    $this->redirect(array('action' => 'index'));
+   } else {
+    $this->Session->setFlash(__('The feed could not be saved. Please, try again.', true));
+   }
+  }
+  if (empty($this->data)) {
+   $this->data = $this->Feed->read(null, $id);
+  }
+  $users = $this->Feed->User->find('list');
+  $jobs = $this->Feed->Job->find('list');
+  $this->set(compact('users', 'jobs'));
+ }
 
-	function delete($id = null) {
-		if (!$id) {
-			$this->Session->setFlash(__('Invalid id for feed', true));
-			$this->redirect(array('action'=>'index'));
-		}
-		if ($this->Feed->delete($id)) {
-			$this->Session->setFlash(__('Feed deleted', true));
-			$this->redirect(array('action'=>'index'));
-		}
-		$this->Session->setFlash(__('Feed was not deleted', true));
-		$this->redirect(array('action' => 'index'));
-	}
-	function admin_index() {
-		$this->Feed->recursive = 0;
-		$this->set('feeds', $this->paginate());
-	}
+ function delete($id = null) {
+  if (!$id) {
+   $this->Session->setFlash(__('Invalid id for feed', true));
+   $this->redirect(array('action'=>'index'));
+  }
+  if ($this->Feed->delete($id)) {
+   $this->Session->setFlash(__('Feed deleted', true));
+   $this->redirect(array('action'=>'index'));
+  }
+  $this->Session->setFlash(__('Feed was not deleted', true));
+  $this->redirect(array('action' => 'index'));
+ }
+ function admin_index() {
+  $this->Feed->recursive = 0;
+  $this->set('feeds', $this->paginate());
+ }
 
-	function admin_view($id = null) {
-		if (!$id) {
-			$this->Session->setFlash(__('Invalid feed', true));
-			$this->redirect(array('action' => 'index'));
-		}
-		$this->set('feed', $this->Feed->read(null, $id));
-	}
+ function admin_view($id = null) {
+  if (!$id) {
+   $this->Session->setFlash(__('Invalid feed', true));
+   $this->redirect(array('action' => 'index'));
+  }
+  $this->set('feed', $this->Feed->read(null, $id));
+ }
 
-	function admin_add() {
-		if (!empty($this->data)) {
-			$this->Feed->create();
-			if ($this->Feed->save($this->data)) {
-				$this->Session->setFlash(__('The feed has been saved', true));
-				$this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(__('The feed could not be saved. Please, try again.', true));
-			}
-		}
-		$users = $this->Feed->User->find('list');
-		$jobs = $this->Feed->Job->find('list');
-		$this->set(compact('users', 'jobs'));
-	}
+ function admin_add() {
+  if (!empty($this->data)) {
+   $this->Feed->create();
+   if ($this->Feed->save($this->data)) {
+    $this->Session->setFlash(__('The feed has been saved', true));
+    $this->redirect(array('action' => 'index'));
+   } else {
+    $this->Session->setFlash(__('The feed could not be saved. Please, try again.', true));
+   }
+  }
+  $users = $this->Feed->User->find('list');
+  $jobs = $this->Feed->Job->find('list');
+  $this->set(compact('users', 'jobs'));
+ }
 
-	function admin_edit($id = null) {
-		if (!$id && empty($this->data)) {
-			$this->Session->setFlash(__('Invalid feed', true));
-			$this->redirect(array('action' => 'index'));
-		}
-		if (!empty($this->data)) {
-			if ($this->Feed->save($this->data)) {
-				$this->Session->setFlash(__('The feed has been saved', true));
-				$this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(__('The feed could not be saved. Please, try again.', true));
-			}
-		}
-		if (empty($this->data)) {
-			$this->data = $this->Feed->read(null, $id);
-		}
-		$users = $this->Feed->User->find('list');
-		$jobs = $this->Feed->Job->find('list');
-		$this->set(compact('users', 'jobs'));
-	}
+ function admin_edit($id = null) {
+  if (!$id && empty($this->data)) {
+   $this->Session->setFlash(__('Invalid feed', true));
+   $this->redirect(array('action' => 'index'));
+  }
+  if (!empty($this->data)) {
+   if ($this->Feed->save($this->data)) {
+    $this->Session->setFlash(__('The feed has been saved', true));
+    $this->redirect(array('action' => 'index'));
+   } else {
+    $this->Session->setFlash(__('The feed could not be saved. Please, try again.', true));
+   }
+  }
+  if (empty($this->data)) {
+   $this->data = $this->Feed->read(null, $id);
+  }
+  $users = $this->Feed->User->find('list');
+  $jobs = $this->Feed->Job->find('list');
+  $this->set(compact('users', 'jobs'));
+ }
 
-	function admin_delete($id = null) {
-		if (!$id) {
-			$this->Session->setFlash(__('Invalid id for feed', true));
-			$this->redirect(array('action'=>'index'));
-		}
-		if ($this->Feed->delete($id)) {
-			$this->Session->setFlash(__('Feed deleted', true));
-			$this->redirect(array('action'=>'index'));
-		}
-		$this->Session->setFlash(__('Feed was not deleted', true));
-		$this->redirect(array('action' => 'index'));
-	}
+ function admin_delete($id = null) {
+  if (!$id) {
+   $this->Session->setFlash(__('Invalid id for feed', true));
+   $this->redirect(array('action'=>'index'));
+  }
+  if ($this->Feed->delete($id)) {
+   $this->Session->setFlash(__('Feed deleted', true));
+   $this->redirect(array('action'=>'index'));
+  }
+  $this->Session->setFlash(__('Feed was not deleted', true));
+  $this->redirect(array('action' => 'index'));
+ }
 
-	function timeline() {
-		$id = $this->Auth->user('id');
-		if (!$id) {
-			$this->Session->setFlash(__('Invalid feed', true));
-			$this->redirect(array('action' => 'index'));
-		}
-		$this->set('feeds', $this->paginate());
-		$friends = $this->Friend->find('all', array('conditions'=> array('user_id'=>$id)));
-		
-		$feed_id = '';
-		foreach ($friends as $val) {
-			$feed_id .= $val['Friend']['friend_id'].',';
-		}
-		$feed_id = $feed_id.$id;
-		$timeline = $this->Feed->find('all', array('order' => 'Feed.created DESC','conditions'=> array("Feed.user_id IN ($feed_id)")));
-		$this->set(compact('timeline'));
-	}
-	
-	function detail($feed_id) {
-		if (!$feed_id) {
-			$this->Session->setFlash(__('Invalid feed', true));
-			$this->redirect(array('action' => 'index'));
-		}
-		$this->set('feeds', $this->paginate());
-		$detail = $this->Feed->find('first', array('conditions'=> array("Feed.id" => $feed_id)));
-		$likes  = $this->Like->find('all', array('conditions'=> array("Like.feed_id" => $feed_id)));
-		$this->set(compact('detail', 'likes'));
-	}
+ function timeline() {
+  $id = $this->Auth->user('id');
+  if (!$id) {
+   $this->Session->setFlash(__('Invalid feed', true));
+   $this->redirect(array('action' => 'index'));
+  }
+  $this->set('feeds', $this->paginate());
+  $friends = $this->Friend->find('all', array('conditions'=> array('user_id'=>$id)));
+
+  $feed_id = '';
+  foreach ($friends as $val) {
+   $feed_id .= $val['Friend']['friend_id'].',';
+  }
+  $feed_id = $feed_id.$id;
+  $timeline = $this->Feed->find('all', array('order' => 'Feed.created DESC','conditions'=> array("Feed.user_id IN ($feed_id)")));
+  $this->set(compact('timeline'));
+ }
+
+ function detail($feed_id) {
+  if (!$feed_id) {
+   $this->Session->setFlash(__('Invalid feed', true));
+   $this->redirect(array('action' => 'index'));
+  }
+  $this->set('feeds', $this->paginate());
+  $detail = $this->Feed->find('first', array('conditions'=> array("Feed.id" => $feed_id)));
+  $likes  = $this->Like->find('all', array('conditions'=> array("Like.feed_id" => $feed_id)));
+  $this->set(compact('detail', 'likes'));
+ }
 }
--- app/controllers/jobs_controller.php
+++ app/controllers/jobs_controller.php
@@ -3,8 +3,8 @@ class JobsController extends AppController {
 
     var $name = 'Jobs';
     var $uses = array('Job', 'Feed');
-	var $helpers = array('Javascript');
-    var $components = array('Auth');
+ var $helpers = array('Javascript');
+    var $components = array('Auth', 'WebApi');
 
     function beforeFilter(){
         $this->Auth->userModel = 'User';
@@ -16,185 +16,186 @@ class JobsController extends AppController {
         $this->Auth->authError = 'Please try logon as admin.';
     }
 
-	function index() {
-		$this->Job->recursive = 0;
-		$this->set('jobs', $this->paginate());
-	}
+ function index() {
+  $this->Job->recursive = 0;
+  $this->set('jobs', $this->paginate());
+ }
 
     // user check in a job.
     function checkin($id = null) {
-		if (!$id) {
-			$this->Session->setFlash(__('Invalid job', true));
-			$this->redirect(array('action' => 'index'));
+  if (!$id) {
+   $this->Session->setFlash(__('Invalid job', true));
+   $this->redirect(array('action' => 'index'));
         }
         $job = $this->Job->read(null, $id);
         if (!empty($job)) {
             $job['Job']['checkin'] = DboSource::expression('Now()');
             if ($this->Job->save($job)) {
                 $this->addFeed();
-				$this->Session->setFlash(__('The checkin has been saved', true));
-				$this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(__('The checkin could not be saved. Please, try again.', true));
-			}
-		}
-		if (empty($this->data)) {
-			$this->data = $this->Job->read(null, $id);
-		}
-		$users = $this->Job->User->find('list');
-		$jobkinds = $this->Job->Jobkind->find('list');
-		$this->set(compact('users', 'jobkinds'));
+    $this->Session->setFlash(__('The checkin has been saved', true));
+    $this->redirect(array('action' => 'index'));
+   } else {
+    $this->Session->setFlash(__('The checkin could not be saved. Please, try again.', true));
+   }
+  }
+  if (empty($this->data)) {
+   $this->data = $this->Job->read(null, $id);
+  }
+  $users = $this->Job->User->find('list');
+  $jobkinds = $this->Job->Jobkind->find('list');
+  $this->set(compact('users', 'jobkinds'));
     }
 
     // user check out a job.
     function checkout($id = null) {
-		if (!$id) {
-			$this->Session->setFlash(__('Invalid job', true));
-			$this->redirect(array('action' => 'index'));
+  if (!$id) {
+   $this->Session->setFlash(__('Invalid job', true));
+   $this->redirect(array('action' => 'index'));
         }
         $job = $this->Job->read(null, $id);
         if (!empty($job)) {
             $job['Job']['checkout'] = DboSource::expression('Now()');
             if ($this->Job->save($job)) {
                 $this->addFeed();
-				$this->Session->setFlash(__('The checkout has been saved', true));
-				$this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(__('The checkout could not be saved. Please, try again.', true));
-			}
-		}
-		if (empty($this->data)) {
-			$this->data = $this->Job->read(null, $id);
-		}
-		$users = $this->Job->User->find('list');
-		$jobkinds = $this->Job->Jobkind->find('list');
-		$this->set(compact('users', 'jobkinds'));
+    $this->Session->setFlash(__('The checkout has been saved', true));
+    $this->redirect(array('action' => 'index'));
+   } else {
+    $this->Session->setFlash(__('The checkout could not be saved. Please, try again.', true));
+   }
+  }
+  if (empty($this->data)) {
+   $this->data = $this->Job->read(null, $id);
+  }
+  $users = $this->Job->User->find('list');
+  $jobkinds = $this->Job->Jobkind->find('list');
+  $this->set(compact('users', 'jobkinds'));
     }
 
-	function view($id = null) {
-		if (!$id) {
-			$this->Session->setFlash(__('Invalid job', true));
-			$this->redirect(array('action' => 'index'));
-		}
-		$this->set('job', $this->Job->read(null, $id));
-	}
+ function view($id = null) {
+  if (!$id) {
+   $this->Session->setFlash(__('Invalid job', true));
+   $this->redirect(array('action' => 'index'));
+  }
+  $this->set('job', $this->Job->read(null, $id));
+ }
 
     function add() {
         $this->layout = '';
-		if (!empty($this->data)) {
-			$this->Job->create();
-			if ($this->Job->save($this->data)) {
-				$this->Session->setFlash(__('The job has been saved', true));
-				$this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(__('The job could not be saved. Please, try again.', true));
-			}
-		}
-		$jobkinds = $this->Job->Jobkind->find('list');
-		$this->set(compact('jobkinds'));
+  if (!empty($this->data)) {
+   $this->Job->create();
+   if ($this->Job->save($this->data)) {
+    $this->Session->setFlash(__('The job has been saved', true));
+    $this->redirect(array('action' => 'index'));
+   } else {
+    $this->Session->setFlash(__('The job could not be saved. Please, try again.', true));
+   }
+  }
+  $jobkinds = $this->Job->Jobkind->find('list');
+  $this->set(compact('jobkinds'));
         $this->set('user', $this->Job->User->read(null, $this->Auth->user('id')));
-	}
+ }
 
-	function edit($id = null) {
-		if (!$id && empty($this->data)) {
-			$this->Session->setFlash(__('Invalid job', true));
-			$this->redirect(array('action' => 'index'));
-		}
-		if (!empty($this->data)) {
-			if ($this->Job->save($this->data)) {
-				$this->Session->setFlash(__('The job has been saved', true));
-				$this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(__('The job could not be saved. Please, try again.', true));
-			}
-		}
-		if (empty($this->data)) {
-			$this->data = $this->Job->read(null, $id);
-		}
-		$users = $this->Job->User->find('list');
-		$jobkinds = $this->Job->Jobkind->find('list');
-		$this->set(compact('users', 'jobkinds'));
-	}
+ function edit($id = null) {
+  if (!$id && empty($this->data)) {
+   $this->Session->setFlash(__('Invalid job', true));
+   $this->redirect(array('action' => 'index'));
+  }
+  if (!empty($this->data)) {
+   if ($this->Job->save($this->data)) {
+    $this->Session->setFlash(__('The job has been saved', true));
+    $this->redirect(array('action' => 'index'));
+   } else {
+    $this->Session->setFlash(__('The job could not be saved. Please, try again.', true));
+   }
+  }
+  if (empty($this->data)) {
+   $this->data = $this->Job->read(null, $id);
+  }
+  $users = $this->Job->User->find('list');
+  $jobkinds = $this->Job->Jobkind->find('list');
+  $this->set(compact('users', 'jobkinds'));
+ }
 
-	function delete($id = null) {
-		if (!$id) {
-			$this->Session->setFlash(__('Invalid id for job', true));
-			$this->redirect(array('action'=>'index'));
-		}
-		if ($this->Job->delete($id)) {
-			$this->Session->setFlash(__('Job deleted', true));
-			$this->redirect(array('action'=>'index'));
-		}
-		$this->Session->setFlash(__('Job was not deleted', true));
-		$this->redirect(array('action' => 'index'));
+ function delete($id = null) {
+  if (!$id) {
+   $this->Session->setFlash(__('Invalid id for job', true));
+   $this->redirect(array('action'=>'index'));
+  }
+  if ($this->Job->delete($id)) {
+   $this->Session->setFlash(__('Job deleted', true));
+   $this->redirect(array('action'=>'index'));
+  }
+  $this->Session->setFlash(__('Job was not deleted', true));
+  $this->redirect(array('action' => 'index'));
     }
 
     // add Feed data for SNS TimeLines
     function addFeed($data =null) {
+        // Add Feed for message
         return false;
     }
 
     /*** Admin Controllers ***/
 
-	function admin_index() {
-		$this->Job->recursive = 0;
-		$this->set('jobs', $this->paginate());
-	}
+ function admin_index() {
+  $this->Job->recursive = 0;
+  $this->set('jobs', $this->paginate());
+ }
 
-	function admin_view($id = null) {
-		if (!$id) {
-			$this->Session->setFlash(__('Invalid job', true));
-			$this->redirect(array('action' => 'index'));
-		}
-		$this->set('job', $this->Job->read(null, $id));
-	}
+ function admin_view($id = null) {
+  if (!$id) {
+   $this->Session->setFlash(__('Invalid job', true));
+   $this->redirect(array('action' => 'index'));
+  }
+  $this->set('job', $this->Job->read(null, $id));
+ }
 
-	function admin_add() {
-		if (!empty($this->data)) {
-			$this->Job->create();
-			if ($this->Job->save($this->data)) {
-				$this->Session->setFlash(__('The job has been saved', true));
-				$this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(__('The job could not be saved. Please, try again.', true));
-			}
-		}
-		$users = $this->Job->User->find('list');
-		$jobkinds = $this->Job->Jobkind->find('list');
-		$this->set(compact('users', 'jobkinds'));
-	}
+ function admin_add() {
+  if (!empty($this->data)) {
+   $this->Job->create();
+   if ($this->Job->save($this->data)) {
+    $this->Session->setFlash(__('The job has been saved', true));
+    $this->redirect(array('action' => 'index'));
+   } else {
+    $this->Session->setFlash(__('The job could not be saved. Please, try again.', true));
+   }
+  }
+  $users = $this->Job->User->find('list');
+  $jobkinds = $this->Job->Jobkind->find('list');
+  $this->set(compact('users', 'jobkinds'));
+ }
 
-	function admin_edit($id = null) {
-		if (!$id && empty($this->data)) {
-			$this->Session->setFlash(__('Invalid job', true));
-			$this->redirect(array('action' => 'index'));
-		}
-		if (!empty($this->data)) {
-			if ($this->Job->save($this->data)) {
-				$this->Session->setFlash(__('The job has been saved', true));
-				$this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(__('The job could not be saved. Please, try again.', true));
-			}
-		}
-		if (empty($this->data)) {
-			$this->data = $this->Job->read(null, $id);
-		}
-		$users = $this->Job->User->find('list');
-		$jobkinds = $this->Job->Jobkind->find('list');
-		$this->set(compact('users', 'jobkinds'));
-	}
+ function admin_edit($id = null) {
+  if (!$id && empty($this->data)) {
+   $this->Session->setFlash(__('Invalid job', true));
+   $this->redirect(array('action' => 'index'));
+  }
+  if (!empty($this->data)) {
+   if ($this->Job->save($this->data)) {
+    $this->Session->setFlash(__('The job has been saved', true));
+    $this->redirect(array('action' => 'index'));
+   } else {
+    $this->Session->setFlash(__('The job could not be saved. Please, try again.', true));
+   }
+  }
+  if (empty($this->data)) {
+   $this->data = $this->Job->read(null, $id);
+  }
+  $users = $this->Job->User->find('list');
+  $jobkinds = $this->Job->Jobkind->find('list');
+  $this->set(compact('users', 'jobkinds'));
+ }
 
-	function admin_delete($id = null) {
-		if (!$id) {
-			$this->Session->setFlash(__('Invalid id for job', true));
-			$this->redirect(array('action'=>'index'));
-		}
-		if ($this->Job->delete($id)) {
-			$this->Session->setFlash(__('Job deleted', true));
-			$this->redirect(array('action'=>'index'));
-		}
-		$this->Session->setFlash(__('Job was not deleted', true));
-		$this->redirect(array('action' => 'index'));
-	}
+ function admin_delete($id = null) {
+  if (!$id) {
+   $this->Session->setFlash(__('Invalid id for job', true));
+   $this->redirect(array('action'=>'index'));
+  }
+  if ($this->Job->delete($id)) {
+   $this->Session->setFlash(__('Job deleted', true));
+   $this->redirect(array('action'=>'index'));
+  }
+  $this->Session->setFlash(__('Job was not deleted', true));
+  $this->redirect(array('action' => 'index'));
+ }
 }
