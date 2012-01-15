fb48c4dca47cf74ccc575a80846aab1c5533c606
Hidetaro7 <hidetaro7@gmail.com>
2012-01-15 15:43:38 +0900

    新規登録画面、CSSとJSを分離

--- mockup/source/js/global.js
+++ mockup/source/js/global.js
@@ -1,4 +1,6 @@
 
+//Grobal（総合） ----------------------------------------------------------------------
+
 Global = {
 	
 	init: function (){
@@ -16,9 +18,41 @@ Global = {
 			return this;
 		}
 	
-	};
-	
+	}; // Grobal end
+
+
+//Grobal（総合） ----------------------------------------------------------------------
 
 	
-// Activate
-window.onload = Global.init;
+	//新規登録 ----------------------------------------------------------------------
+	
+	var Resist = {
+			check: function (){
+					if($("body").hasClass("nickname")) Resist.registBook();
+				},
+			registBook: function () {
+				var inp = $("#resistration #_name");
+				inp.bind("change", function (){
+						$(".nickname #yourname").text($(this).val());
+					$("#bookArea").css({"background":"none"});
+					var timers = setTimeout(function (){
+							$("#bookArea").css({"background":"url(img/regist_book_bg.jpg) no-repeat"});
+						}, 30);
+					});
+				}
+		}//Resist end
+
+	//新規登録 ----------------------------------------------------------------------
+
+
+	
+	//アクティベート（実行） ----------------------------------------------------------------------
+	window.onload = function (){
+		
+		// Grobal Activate
+		Global.init();
+		
+		//新規登録アクティベート
+		Resist.check();
+		
+	}
