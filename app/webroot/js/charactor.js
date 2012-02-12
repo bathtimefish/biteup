2e1dadd5a9881d45007f7a18a11f9a237b0b370b
BathTimeFish <bathtimefish@gmail.com>
2012-02-12 16:39:24 +0900

    fixed

--- app/webroot/js/charactor.js
+++ app/webroot/js/charactor.js
@@ -1,212 +1,212 @@
 var Charactor = {
-	chara: {
-		power: {
-			name: "力仕事系、労働",
-			level: [
-						{ lv:"0", url:"chara_power_lv0.png", alt: "ただの平凡なフェアリー" },
-						{ lv:"1", url:"chara_power_lv1.png", alt: "引越しスタッフのピクシー" },
-						{ lv:"2", url:"chara_power_lv2.png", alt: "建築現場のゴブリン" },
-						{ lv:"3", url:"chara_power_lv3.png", alt: "現場チーフのエルフ" },
-						{ lv:"4", url:"chara_power_lv4.png", alt: "筋肉ムキムキになったドワーフ" },
-						{ lv:"5", url:"chara_power_lv5.png", alt: "" }
-					]
-				}, // 職業：power　終了
-				
-		events: {
-			name: "イベントスタッフ",
-			level: [
-						{ lv:"0", url:"chara_event_lv0.png", alt: "ただの平凡なフェアリー" },
-						{ lv:"1", url:"chara_event_lv1.png", alt: "ティッシュ配りのピクシー" },
-						{ lv:"2", url:"chara_event_lv2.png", alt: "スポーツ会場警備のゴブリン" },
-						{ lv:"3", url:"chara_event_lv3.png", alt: "コンサートスタッフのエルフ" },
-						{ lv:"4", url:"chara_event_lv4.png", alt: "モーターショーのドワーフ" },
-						{ lv:"5", url:"chara_event_lv5.png", alt: "" }
-					]
-				}, // 職業：events　終了
-				
-		sampling: {
-			name: "サンプリングスタッフ",
-			level: [
-						{ lv:"0", url:"chara_sampling_lv0.png", alt: "ただの平凡なフェアリー" },
-						{ lv:"1", url:"chara_sampling_lv1.png", alt: "チラシ配りのピクシー" },
-						{ lv:"2", url:"chara_sampling_lv2.png", alt: "ソーセージ試食のゴブリン" },
-						{ lv:"3", url:"chara_sampling_lv3.png", alt: "コーヒー試飲のエルフ" },
-						{ lv:"4", url:"chara_sampling_lv4.png", alt: "ワイン試飲のほろ酔いドワーフ" },
-						{ lv:"5", url:"chara_sampling_lv5.png", alt: "" }
-					]
-				}, // 職業：sampling　終了
-				
-		shop: {
-			name: "店舗スタッフ",
-			level: [
-						{ lv:"0", url:"chara_shop_lv0.png", alt: "ただの平凡なフェアリー" },
-						{ lv:"1", url:"chara_shop_lv1.png", alt: "販売員のピクシー" },
-						{ lv:"2", url:"chara_shop_lv2.png", alt: "チーフのゴブリン" },
-						{ lv:"3", url:"chara_shop_lv3.png", alt: "フロアマネージャーのエルフ" },
-						{ lv:"4", url:"chara_shop_lv4.png", alt: "店長のドワーフ" },
-						{ lv:"5", url:"chara_shop_lv5.png", alt: "" }
-					]
-				}, // 職業：shop　終了
-				
-		office: {
-			name: "オフィスワーク",
-			level: [
-						{ lv:"0", url:"chara_office_lv0.png", alt: "ただの平凡なフェアリー" },
-						{ lv:"1", url:"chara_office_lv1.png", alt: "新人事務のピクシー" },
-						{ lv:"2", url:"chara_office_lv2.png", alt: "営業事務のゴブリン" },
-						{ lv:"3", url:"chara_office_lv3.png", alt: "経理で何かを企むエルフ" },
-						{ lv:"4", url:"chara_office_lv4.png", alt: "企画のドワーフ" },
-						{ lv:"5", url:"chara_office_lv5.png", alt: "" }
-					]
-				}, // 職業：office　終了
-				
-		teacher: {
-			name: "講師・インストラクター",
-			level: [
-						{ lv:"0", url:"chara_teacher_lv0.png", alt: "ただの平凡なフェアリー" },
-						{ lv:"1", url:"chara_teacher_lv1.png", alt: "アシスタントのピクシー" },
-						{ lv:"2", url:"chara_teacher_lv2.png", alt: "塾講師ののゴブリン" },
-						{ lv:"3", url:"chara_teacher_lv3.png", alt: "英会話教室のエルフ" },
-						{ lv:"4", url:"chara_teacher_lv4.png", alt: "セミナー講師のドワーフ" },
-						{ lv:"5", url:"chara_teacher_lv5.png", alt: "" }
-					]
-				}, // 職業：teacher　終了
-				
-		free: {
-			name: "遊び人（その他）",
-			level: [
-						{ lv:"0", url:"chara_free_lv0.png", alt: "ただの平凡なフェアリー" },
-						{ lv:"1", url:"chara_free_lv1.png", alt: "ヘルプのピクシー" },
-						{ lv:"2", url:"chara_free_lv2.png", alt: "夜のお店のゴブリン" },
-						{ lv:"3", url:"chara_free_lv3.png", alt: "悪そうな奴は大体友達なエルフ" },
-						{ lv:"4", url:"chara_free_lv4.png", alt: "夜の女王ドワーフ" },
-						{ lv:"5", url:"chara_free_lv5.png", alt: "" }
-					]
-				}, // 職業：free　終了
-				
-		season: {
-			name: "季節系（夏季限定／冬季限定）",
-			level: [
-						{ lv:"0", url:"chara_season_lv0.png", alt: "ただの平凡なフェアリー" },
-						{ lv:"1", url:"chara_season_lv1.png", alt: "海の家のピクシー" },
-						{ lv:"2", url:"chara_season_lv2.png", alt: "山小屋のゴブリン" },
-						{ lv:"3", url:"chara_season_lv3.png", alt: "スキーインストラクターのエルフ" },
-						{ lv:"4", url:"chara_season_lv4.png", alt: "ライフセーバーのドワーフ" },
-						{ lv:"5", url:"chara_season_lv5.png", alt: "" }
-					]
-				} // 職業：season　終了
-				
-		}, // chara
-		
-		// --------------------------------   chara　終了   --------------------------------
-		
-	background: [
-			{ url: "bg_lv0.jpg" },
-			{ url: "bg_lv1.jpg" },
-			{ url: "bg_lv2.jpg" },
-			{ url: "bg_lv3.jpg" },
-			{ url: "bg_lv4.jpg" },
-			{ url: "bg_lv5.jpg" }
-		],
-		
-		// --------------------------------   背景　終了   --------------------------------
-		
-	commonTitle: [
-			"ノーマル", "羽が生えてる",	"ツノ／コンボウ",	"耳長／帽子",	"目付き悪／ヒゲ",	"背後キラキラ？"
-		],
-		
-		// --------------------------------   共通特徴　終了   --------------------------------
-	
-		
-	num2JobTitle: function (num){
-			var titles = ["power","events","sampling","shop","office","teacher","free","season"];
-			return titles[num];
-		},
-		
-		// --------------------------------   サーバから来た職業数値を文字列にして返す　終了   --------------------------------
-	
-	thumbBGColor: function (num){
-			var color = ["cyan","yellow","orange","green","parple","silver"]; //追加可能
-			return color[num];
-		},
-		
-		// --------------------------------   サムネイルの背景色はレベルによって変更される　終了   --------------------------------
-	
-	getArray: function (jobKind, level, num){
-		//ここで重ねる画像の配列を返す
-		//引数が0: 背景付き
-		//引数が1: 背景無し
-		//つまり 0だとマイキャラ、1だとサムネイル
-		
-		var imgs = {};
-		
-		imgs.bg = Charactor.background[level].url;
-		
-		if(level === 0) {
-			imgs.uwamono = "chara_lv0.png";
-		}else{
-		$(Charactor.chara).each( function (i, val){
-				for(var i in this) {
-					if(i === jobKind) {
-						imgs.uwamono = this[i].level[level].url;
-						break;
-					}
-				}
-			});
-		}
-		
-		if(num === 1) {
-				//背景無し
-				return ["chara_lv0.png", imgs.uwamono];
-			}else{
-				//背景あり
-				return [imgs.bg, "chara_lv0.png", imgs.uwamono];
-			}
-					
-		},
-	
-	getImage: function (jobKind, level){
-		var basePath = "/img/avatar/";
-		var arr = Charactor.getArray(jobKind, level, 0);
-		
-		var container = $("<div />");
-		var bg = $("<img src='"+basePath+arr[0]+"'>");
-		var uwamono = $("<img src='"+basePath+arr[2]+"'>");
-		var bodys = $("<img src='"+basePath+"chara_lv0.png"+"'>");
-		container.append(bg);
-		if(level === 0) {
-			container.append(bodys);
-		}else{
-			container.append(bodys);
-			container.append(uwamono);
-		}
-		
-		return container[0].innerHTML;
-			
-	},
-	
-	getThumbnail: function (jobKind, level, canvas){
-		//サムネイルにはcanvasで生成する
-		var basePath = "/img/avatar/";
-		var arr = Charactor.getArray(Charactor.num2JobTitle(jobKind), level, 1);
-		var ctx = canvas.getContext("2d");
-		var img1 = new Image();
-		img1.src = basePath+arr[0]+"?"+new Date().getTime();
-		img1.onload = function () {
-			var scale = 80/img1.height;
-			var img2 = new Image();
-			img2.src = basePath+arr[1]+"?"+new Date().getTime();
-			img2.onload = function () {
-				ctx.fillStyle = Charactor.thumbBGColor(level);
-				ctx.fillRect(0,0,80,80);
-				ctx.setTransform(scale, 0, 0, scale, -30, 0);
-				ctx.drawImage(img1, 0, 0);
-				ctx.drawImage(img2, 0, 0);
-				}
-			
-		}
-		
-	}
+ chara: {
+  power: {
+   name: "力仕事系、労働",
+   level: [
+      { lv:"0", url:"chara_power_lv0.png", alt: "ただの平凡なフェアリー" },
+      { lv:"1", url:"chara_power_lv1.png", alt: "引越しスタッフのピクシー" },
+      { lv:"2", url:"chara_power_lv2.png", alt: "建築現場のゴブリン" },
+      { lv:"3", url:"chara_power_lv3.png", alt: "現場チーフのエルフ" },
+      { lv:"4", url:"chara_power_lv4.png", alt: "筋肉ムキムキになったドワーフ" },
+      { lv:"5", url:"chara_power_lv5.png", alt: "" }
+     ]
+    }, // 職業：power　終了
+
+  events: {
+   name: "イベントスタッフ",
+   level: [
+      { lv:"0", url:"chara_event_lv0.png", alt: "ただの平凡なフェアリー" },
+      { lv:"1", url:"chara_event_lv1.png", alt: "ティッシュ配りのピクシー" },
+      { lv:"2", url:"chara_event_lv2.png", alt: "スポーツ会場警備のゴブリン" },
+      { lv:"3", url:"chara_event_lv3.png", alt: "コンサートスタッフのエルフ" },
+      { lv:"4", url:"chara_event_lv4.png", alt: "モーターショーのドワーフ" },
+      { lv:"5", url:"chara_event_lv5.png", alt: "" }
+     ]
+    }, // 職業：events　終了
+
+  sampling: {
+   name: "サンプリングスタッフ",
+   level: [
+      { lv:"0", url:"chara_sampling_lv0.png", alt: "ただの平凡なフェアリー" },
+      { lv:"1", url:"chara_sampling_lv1.png", alt: "チラシ配りのピクシー" },
+      { lv:"2", url:"chara_sampling_lv2.png", alt: "ソーセージ試食のゴブリン" },
+      { lv:"3", url:"chara_sampling_lv3.png", alt: "コーヒー試飲のエルフ" },
+      { lv:"4", url:"chara_sampling_lv4.png", alt: "ワイン試飲のほろ酔いドワーフ" },
+      { lv:"5", url:"chara_sampling_lv5.png", alt: "" }
+     ]
+    }, // 職業：sampling　終了
+
+  shop: {
+   name: "店舗スタッフ",
+   level: [
+      { lv:"0", url:"chara_shop_lv0.png", alt: "ただの平凡なフェアリー" },
+      { lv:"1", url:"chara_shop_lv1.png", alt: "販売員のピクシー" },
+      { lv:"2", url:"chara_shop_lv2.png", alt: "チーフのゴブリン" },
+      { lv:"3", url:"chara_shop_lv3.png", alt: "フロアマネージャーのエルフ" },
+      { lv:"4", url:"chara_shop_lv4.png", alt: "店長のドワーフ" },
+      { lv:"5", url:"chara_shop_lv5.png", alt: "" }
+     ]
+    }, // 職業：shop　終了
+
+  office: {
+   name: "オフィスワーク",
+   level: [
+      { lv:"0", url:"chara_office_lv0.png", alt: "ただの平凡なフェアリー" },
+      { lv:"1", url:"chara_office_lv1.png", alt: "新人事務のピクシー" },
+      { lv:"2", url:"chara_office_lv2.png", alt: "営業事務のゴブリン" },
+      { lv:"3", url:"chara_office_lv3.png", alt: "経理で何かを企むエルフ" },
+      { lv:"4", url:"chara_office_lv4.png", alt: "企画のドワーフ" },
+      { lv:"5", url:"chara_office_lv5.png", alt: "" }
+     ]
+    }, // 職業：office　終了
+
+  teacher: {
+   name: "講師・インストラクター",
+   level: [
+      { lv:"0", url:"chara_teacher_lv0.png", alt: "ただの平凡なフェアリー" },
+      { lv:"1", url:"chara_teacher_lv1.png", alt: "アシスタントのピクシー" },
+      { lv:"2", url:"chara_teacher_lv2.png", alt: "塾講師ののゴブリン" },
+      { lv:"3", url:"chara_teacher_lv3.png", alt: "英会話教室のエルフ" },
+      { lv:"4", url:"chara_teacher_lv4.png", alt: "セミナー講師のドワーフ" },
+      { lv:"5", url:"chara_teacher_lv5.png", alt: "" }
+     ]
+    }, // 職業：teacher　終了
+
+  free: {
+   name: "遊び人（その他）",
+   level: [
+      { lv:"0", url:"chara_free_lv0.png", alt: "ただの平凡なフェアリー" },
+      { lv:"1", url:"chara_free_lv1.png", alt: "ヘルプのピクシー" },
+      { lv:"2", url:"chara_free_lv2.png", alt: "夜のお店のゴブリン" },
+      { lv:"3", url:"chara_free_lv3.png", alt: "悪そうな奴は大体友達なエルフ" },
+      { lv:"4", url:"chara_free_lv4.png", alt: "夜の女王ドワーフ" },
+      { lv:"5", url:"chara_free_lv5.png", alt: "" }
+     ]
+    }, // 職業：free　終了
+
+  season: {
+   name: "季節系（夏季限定／冬季限定）",
+   level: [
+      { lv:"0", url:"chara_season_lv0.png", alt: "ただの平凡なフェアリー" },
+      { lv:"1", url:"chara_season_lv1.png", alt: "海の家のピクシー" },
+      { lv:"2", url:"chara_season_lv2.png", alt: "山小屋のゴブリン" },
+      { lv:"3", url:"chara_season_lv3.png", alt: "スキーインストラクターのエルフ" },
+      { lv:"4", url:"chara_season_lv4.png", alt: "ライフセーバーのドワーフ" },
+      { lv:"5", url:"chara_season_lv5.png", alt: "" }
+     ]
+    } // 職業：season　終了
+
+  }, // chara
+
+  // --------------------------------   chara　終了   --------------------------------
+
+ background: [
+   { url: "bg_lv0.jpg" },
+   { url: "bg_lv1.jpg" },
+   { url: "bg_lv2.jpg" },
+   { url: "bg_lv3.jpg" },
+   { url: "bg_lv4.jpg" },
+   { url: "bg_lv5.jpg" }
+  ],
+
+  // --------------------------------   背景　終了   --------------------------------
+
+ commonTitle: [
+   "ノーマル", "羽が生えてる", "ツノ／コンボウ", "耳長／帽子", "目付き悪／ヒゲ", "背後キラキラ？"
+  ],
+
+  // --------------------------------   共通特徴　終了   --------------------------------
+
+
+ num2JobTitle: function (num){
+   var titles = ["power","events","sampling","shop","office","teacher","free","season"];
+   return titles[num];
+  },
+
+  // --------------------------------   サーバから来た職業数値を文字列にして返す　終了   --------------------------------
+
+ thumbBGColor: function (num){
+   var color = ["cyan","yellow","orange","green","parple","silver"]; //追加可能
+   return color[num];
+  },
+
+  // --------------------------------   サムネイルの背景色はレベルによって変更される　終了   --------------------------------
+
+ getArray: function (jobKind, level, num){
+  //ここで重ねる画像の配列を返す
+  //引数が0: 背景付き
+  //引数が1: 背景無し
+  //つまり 0だとマイキャラ、1だとサムネイル
+
+  var imgs = {};
+
+  imgs.bg = Charactor.background[level].url;
+
+  if(level === 0) {
+   imgs.uwamono = "chara_lv0.png";
+  }else{
+  $(Charactor.chara).each( function (i, val){
+    for(var i in this) {
+     if(i === jobKind) {
+      imgs.uwamono = this[i].level[level].url;
+      break;
+     }
+    }
+   });
+  }
+
+  if(num === 1) {
+    //背景無し
+    return ["chara_lv0.png", imgs.uwamono];
+   }else{
+    //背景あり
+    return [imgs.bg, "chara_lv0.png", imgs.uwamono];
+   }
+
+  },
+
+ getImage: function (jobKind, level){
+  var basePath = "/a/img/avatar/";
+  var arr = Charactor.getArray(jobKind, level, 0);
+
+  var container = $("<div />");
+  var bg = $("<img src='"+basePath+arr[0]+"'>");
+  var uwamono = $("<img src='"+basePath+arr[2]+"'>");
+  var bodys = $("<img src='"+basePath+"chara_lv0.png"+"'>");
+  container.append(bg);
+  if(level === 0) {
+   container.append(bodys);
+  }else{
+   container.append(bodys);
+   container.append(uwamono);
+  }
+
+  return container[0].innerHTML;
+
+ },
+
+ getThumbnail: function (jobKind, level, canvas){
+  //サムネイルにはcanvasで生成する
+  var basePath = "/a/img/avatar/";
+  var arr = Charactor.getArray(Charactor.num2JobTitle(jobKind), level, 1);
+  var ctx = canvas.getContext("2d");
+  var img1 = new Image();
+  img1.src = basePath+arr[0]+"?"+new Date().getTime();
+  img1.onload = function () {
+   var scale = 80/img1.height;
+   var img2 = new Image();
+   img2.src = basePath+arr[1]+"?"+new Date().getTime();
+   img2.onload = function () {
+    ctx.fillStyle = Charactor.thumbBGColor(level);
+    ctx.fillRect(0,0,80,80);
+    ctx.setTransform(scale, 0, 0, scale, -30, 0);
+    ctx.drawImage(img1, 0, 0);
+    ctx.drawImage(img2, 0, 0);
+    }
+
+  }
+
+ }
 
 
 
